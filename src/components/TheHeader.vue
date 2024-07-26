a9f41afd05e4ed2ef2dc935e9402a8baced83267
Rajveer-ITL <rajveer@depasserinfotech.in>
2024-07-27 01:59:29 +0530

    changes added

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex justify-between items-center px-[64px] h-[64px] sticky top-0 backdrop-filter backdrop-blur-lg"
+    class="flex justify-between items-center px-[64px] h-[64px] sticky top-0 backdrop-filter backdrop-blur-lg header"
   >
     <div class="flex items-center justify-between gap-2">
       <baseButton text="STILLS" :isLoading="false" />
@@ -23,4 +23,20 @@
 
 <script setup>
 import baseButton from "./base/baseButton.vue";
+import { gsap } from "gsap";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  gsap.to(".header", {
+    duration: 2,
+    opacity: 1,
+    ease: "power4.out",
+  });
+});
 </script>
+
+<style scoped>
+.header {
+  opacity: 0; /* Ensure header starts hidden */
+}
+</style>
