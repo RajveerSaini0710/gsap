65bed50e5146c82b04d7e25435d61bbcbce8af6c
Rajveer-ITL <rajveer@depasserinfotech.in>
2024-07-26 23:49:37 +0530

    feat: :sparkles: new code

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import TheHeader from "./components/TheHeader.vue";
+</script>
+
+<template>
+  <TheHeader />
+  <router-view v-slot="slotProps">
+    <component :is="slotProps.Component"></component>
+  </router-view>
+</template>
+
+<style>
+body {
+  background-color: #161719;
+  color: #ffffff;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- text-2xl w-full flex items-center justify-center py-4 font-bold -->
+  <div class="font-roslindale" ref="headerElement">
+    {{ theHeader }}
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import { gsap } from "gsap";
+
+const theHeader = ref("The Header");
+const headerElement = ref(null); // Create a ref for the header element
+
+onMounted(() => {
+  // Use GSAP to animate the header
+  gsap.from(headerElement.value, {
+    duration: 1,
+    opacity: 0,
+    scale: 1.5, // Start scaled up
+    y: -50, // Move the element up by 50 pixels
+    ease: "power2.out", // Easing function for a smoother animation
+    onComplete: () => {
+      // Scale down to original size
+      gsap.to(headerElement.value, {
+        duration: 0.5,
+        scale: 1, // Scale to original size
+        ease: "power2.out",
+      });
+    },
+  });
+});
+</script>
