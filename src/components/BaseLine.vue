b2eeb08b20f12f6fd5c1cca092922de9037a582e
Rajveer-ITL <rajveer@depasserinfotech.in>
2024-08-05 20:31:01 +0530

    changes added

--- src/components/BaseLine.vue
+++ src/components/BaseLine.vue
@@ -1,21 +1,11 @@
 <template>
   <div class="base-line w-full bg-[#ff4056] flex justify-center items-center">
     <div
-      class="dark:text-[#ffffff] text-[40px] font-roslindale text-black py-6 font-extrabold"
+      class="dark:text-[#ffffff] text-[36px] font-serif text-black py-6 font-extrabold"
     >
-      {{ commonVariables.darkMode }}
       Elevate Your Next Project With Our Best Heavy Lifting Services
     </div>
   </div>
 </template>
 
-<script setup>
-import { commonVariables } from "../assets/variables/commonVariables";
-</script>
-
-<style>
-.glow {
-  text-shadow: 0 0 5px #ffffff, 0 0 5px #ffffff, 0 0 5px #ff4056,
-    0 0 5px #ff4056, 0 0 5px #ff4056, 0 0 5px #ff4056;
-}
-</style>
+<script setup></script>
